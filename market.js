90a13cdc3b69d57a39434c66d4a4ddcbb030f43a
Geluos <55185142+Geluos@users.noreply.github.com>
2023-12-05 00:10:36 +0300

    Update market.js

--- market.js
+++ market.js
@@ -1,10 +1,10 @@
 class Market{
-	#map;
-	#log;
-	#sum
+	#zakaz;
+	#sum;
 
 	constructor() {
-		this.#log = 0;
+		this.#sum = 0;
+		this.#zakaz = [];
 	}
 
 	updateElem(element, name, price, imageName)
@@ -21,6 +21,7 @@ class Market{
 					this.#sum += price;
 					let buyButton = document.getElementById("buyButton");
 					buyButton.textContent = "Корзина " + this.#sum  + '.00₽';
+					this.#zakaz.push(name);
 				});
 			}
 			if (child.classList[0] == 'imgres')
@@ -32,10 +33,11 @@ class Market{
 
 	createGoods()
 	{
-		window.localStorage.setItem("sum", '0')
-		let arrayPhotos = document.querySelector('input[name=photos]').value;
-		let prices = document.querySelector('input[name=prices]').value;
-		let names = document.querySelector('input[name=names]').value;
+		window.localStorage.setItem("sum", '0');
+		let params = new URLSearchParams(window.location.search);
+		let arrayPhotos = params.get("photos");
+		let prices = params.get("prices");
+		let names = params.get("names");
 		let box = document.getElementById("flexbox");
 		let element = document.getElementById("adam");
 		for (let i = 0; i < arrayPhotos.length; i++) 
@@ -55,7 +57,8 @@ class Market{
 		buyButton.addEventListener("click", () => 
 		{
 			let data = {
-				type: "market"
+				type: "market",
+				goods: this.#zakaz
 			}
 			tg.sendData(JSON.stringify(data));
 
